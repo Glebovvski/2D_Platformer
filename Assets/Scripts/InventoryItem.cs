52640fa76cd44a95a806adae9b184ed44ac8b6c3
Gleb <zadachings@gmail.com>
2018-09-05 17:56:10 +0300

    added inventory layout

---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryItem : MonoBehaviour {
+
+    [SerializeField]
+    private string itemName;
+    [SerializeField]
+    private InventoryType itemType;
+    [SerializeField]
+    private Sprite itemSprite;
+    [SerializeField]
+    private float duration;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+enum InventoryType
+{
+    HealthPotion,
+    Shield,
+    StrengthPotion
+}
