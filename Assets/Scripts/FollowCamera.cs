9b8e5c08bb8cfffe891a86a505585d4877e6d3fe
Gleb <zadachings@gmail.com>
2018-09-14 19:40:55 +0300

    extended the level

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -10,17 +10,20 @@ public class FollowCamera : MonoBehaviour
     public Transform target;
     private Camera camera;
 
+    public bool move;
     
-    private Vector3 LeftBarier;
-    private Vector3 RightBarier;
-    private Vector3 UpBarier;
+    private Transform LeftBarier;
+    private Transform RightBarier;
+    private Transform UpBarier;
 
     private void Start()
     {
-        LeftBarier = GameObject.Find("LeftBarier").GetComponent<Transform>().position +target.position;
-        RightBarier = GameObject.Find("RightBarier").GetComponent<Transform>().position -target.position;
-        UpBarier = GameObject.Find("UpBarier").GetComponent<Transform>().position+target.position;
+        move = true;
+        LeftBarier = GameObject.Find("LeftBarierActual").GetComponent<Transform>();// +target.position;
+        RightBarier = GameObject.Find("RightBarier").GetComponent<Transform>();// -target.position;
+        UpBarier = GameObject.Find("UpBarier").GetComponent<Transform>();// +target.position;
         camera = GetComponent<Camera>();
+        //camera.transform.position = new Vector3(target.position.x,target.position.y,10);
     }
 
     // Update is called once per frame
@@ -30,9 +33,10 @@ public class FollowCamera : MonoBehaviour
         {
             Vector3 point = camera.WorldToViewportPoint(target.position);
             Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.25f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-            Vector3 destination = new Vector3(Mathf.Clamp((transform.position.x + delta.x), LeftBarier.x, RightBarier.x), Mathf.Clamp((transform.position.y + delta.y), -10, UpBarier.y), transform.position.z);
+            Vector3 destination = transform.position + delta;
             //if (destination.x > LeftBarier.position.x-target.position.x && destination.x < RightBarier.position.x-target.position.x || destination.y < UpBarier.position.y - target.position.y)
-            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            //if (camera.transform.position.x > (LeftBarier.position.x+2.04) || camera.transform.position.x < (RightBarier.position.x - 2.04))
+                transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
         }
     }
 }
