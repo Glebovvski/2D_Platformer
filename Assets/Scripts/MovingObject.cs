5a9090cff97283edcbe1e1c745359e232d3683c6
Gleb <zadachings@gmail.com>
2018-09-15 18:49:37 +0300

    created moving platform

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour {
+
+    private float moveTime = 0.5f;
+
+    private float direction = 1;
+    
+    //Protected, virtual functions can be overridden by inheriting classes.
+    protected virtual void Start()
+    {
+    }
+
+
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x + moveTime *Time.smoothDeltaTime * direction, transform.position.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Foreground"))
+            direction *= -1;
+    }
+
+}
