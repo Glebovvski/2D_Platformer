b297e6637d1894983c246c3cf65fe2a547975f51
Gleb <zadachings@gmail.com>
2019-01-24 18:35:36 +0200

    Added scrollable list with scaling selected elements

---
+++ Assets/Scripts/ScrollPanel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollPanel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //StartCoroutine(ScaleUp(other));
+        float progress = 0;
+        Vector3 finalScale = new Vector3(1.3f, 1.3f, 1.3f);
+        //while (progress <= 1)
+        //{
+        //    other.transform.localScale = Vector3.Lerp(other.transform.localScale, finalScale, progress);
+        //    progress += Time.deltaTime * 0.1f;
+        //    yield return null;
+        //}
+        other.transform.localScale = finalScale;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        other.transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        //StartCoroutine(ScaleDown(other));
+        float progress = 0;
+        Vector3 finalScale = new Vector3(1f, 1f, 1f);
+        //while (progress <= 1)
+        //{
+        //    other.transform.localScale = Vector3.Lerp(other.transform.localScale, finalScale, progress);
+        //    progress += Time.deltaTime * 0.1f;
+        //    yield return new WaitForEndOfFrame();
+        //}
+        other.transform.localScale = finalScale;
+    }
+
+    
+
+    IEnumerator ScaleUp(Collider2D other)
+    {
+        float progress = 0;
+        Vector3 finalScale = new Vector3(1.3f, 1.3f, 1.3f);
+        while (progress <= 1)
+        {
+            other.transform.localScale = Vector3.Lerp(other.transform.localScale, finalScale, progress);
+            progress += Time.deltaTime * 0.1f;
+            yield return null;
+        }
+        other.transform.localScale = finalScale;
+    }
+
+   //IEnumerator ScaleDown(Collider2D other)
+   //{
+   //    
+   //}
+}
---
+++ Assets/Scripts/ScrollRectSnap.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class ScrollRectSnap : MonoBehaviour
+{
+    public RectTransform panel;
+    public Button[] buttons;
+    public RectTransform center;
+
+    private float[] distance;
+    private bool dragging = false;
+    private int btnDistance;
+    private int minBtnNum;
+
+    private void Start()
+    {
+        int btnLength = buttons.Length;
+        distance = new float[btnLength];
+
+        btnDistance = (int)Mathf.Abs(buttons[1].GetComponent<RectTransform>().anchoredPosition.x - buttons[0].GetComponent<RectTransform>().anchoredPosition.x);
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            distance[i] = Mathf.Abs(center.transform.position.x - buttons[i].transform.position.x);
+        }
+
+        float minDistance = Mathf.Min(distance);
+
+        for (int j = 0; j < buttons.Length; j++)
+        {
+            if (minDistance == distance[j])
+                minBtnNum = j;
+        }
+
+        if (!dragging)
+        {
+            LerpToButton(minBtnNum * -btnDistance);
+        }
+    }
+
+    void LerpToButton(int position)
+    {
+        float newX = Mathf.Lerp(panel.anchoredPosition.x, position, 3f * Time.deltaTime); //10 looks really good on PC
+
+        Vector2 newPos = new Vector2(newX, panel.anchoredPosition.y);
+
+        panel.anchoredPosition = newPos;
+    }
+
+    public void StartDrag()
+    {
+        dragging = true;
+    }
+
+    public void EndDrag()
+    {
+        dragging = false;
+    }
+
+    
+}
