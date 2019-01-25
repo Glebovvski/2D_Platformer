e6a8a9d07334450f1b5932cc4ff495d6e4071ac9
Gleb <zadachings@gmail.com>
2019-01-25 17:32:23 +0200

    Added details to the level picker button

--- Assets/Scripts/ScrollPanel.cs
+++ Assets/Scripts/ScrollPanel.cs
@@ -7,38 +7,14 @@ public class ScrollPanel : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //StartCoroutine(ScaleUp(other));
-        float progress = 0;
-        Vector3 finalScale = new Vector3(1.3f, 1.3f, 1.3f);
-        //while (progress <= 1)
-        //{
-        //    other.transform.localScale = Vector3.Lerp(other.transform.localScale, finalScale, progress);
-        //    progress += Time.deltaTime * 0.1f;
-        //    yield return null;
-        //}
-        other.transform.localScale = finalScale;
+        StartCoroutine(ScaleUp(other));
     }
-
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        other.transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
-    }
-
+    
     private void OnTriggerExit2D(Collider2D other)
     {
-        //StartCoroutine(ScaleDown(other));
-        float progress = 0;
-        Vector3 finalScale = new Vector3(1f, 1f, 1f);
-        //while (progress <= 1)
-        //{
-        //    other.transform.localScale = Vector3.Lerp(other.transform.localScale, finalScale, progress);
-        //    progress += Time.deltaTime * 0.1f;
-        //    yield return new WaitForEndOfFrame();
-        //}
-        other.transform.localScale = finalScale;
+        //StopAllCoroutines();
+        StartCoroutine(ScaleDown(other));
     }
-
-    
 
     IEnumerator ScaleUp(Collider2D other)
     {
@@ -47,14 +23,22 @@ public class ScrollPanel : MonoBehaviour
         while (progress <= 1)
         {
             other.transform.localScale = Vector3.Lerp(other.transform.localScale, finalScale, progress);
-            progress += Time.deltaTime * 0.1f;
+            progress += Time.deltaTime * 2f;
             yield return null;
         }
         other.transform.localScale = finalScale;
     }
 
-   //IEnumerator ScaleDown(Collider2D other)
-   //{
-   //    
-   //}
+   IEnumerator ScaleDown(Collider2D other)
+   {
+        float progress = 0;
+        Vector3 finalScale = new Vector3(1f, 1f, 1f);
+        while (progress <= 1)
+        {
+            other.transform.localScale = Vector3.Lerp(other.transform.localScale, finalScale, progress);
+            progress += Time.deltaTime * 2f;
+            yield return new WaitForEndOfFrame();
+        }
+        other.transform.localScale = finalScale;
+    }
 }
